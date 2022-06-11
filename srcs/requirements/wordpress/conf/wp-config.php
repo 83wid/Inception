7d15be8d46173f53625817ac2ff2dbf0efa2c0e1
NAWFAL ABOU-ZAHER <nawfal.abouzaher@gmail.com>
2022-06-11 12:35:55 +0100

    🚀 feat: added env vars to wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'nabouzah' );
+define( 'DB_NAME', getenv('MYSQL_DB') );
 
 /** Database username */
-define( 'DB_USER', 'nabouzah' );
+define( 'DB_USER', getenv('MYSQL_USER') );
 
 /** Database password */
-define( 'DB_PASSWORD', 'nabouzah' );
+define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', getenv('MYSQL_HOST') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
